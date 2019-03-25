37d4aadefece69f85948ca6d480b56102bdcba36
Emanuel Freitas <efreitasn@hotmail.com>
2019-03-24 22:10:07 -0300

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 const { resolve } = require('path');
 
+const FEED_FILE = 'rss.xml';
+
 module.exports = {
   siteMetadata: {
     title: `efreitasn`,
     description: `A front-end dev.`,
-    url: 'https://efreitasn.dev',
+    siteUrl: 'https://efreitasn.dev',
+    feedUrl: `https://efreitasn.dev/${FEED_FILE}`,
     author: {
       firstName: 'Emanuel',
       lastName: 'Freitas',
@@ -19,6 +22,89 @@ module.exports = {
     }
   },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                feedUrl
+                author {
+                  fullName
+                }
+              }
+            }
+          }
+        `,
+        setup: ({
+          query: { site }
+        }) => ({
+          title: site.siteMetadata.title,
+          description: site.siteMetadata.description,
+          site_url: site.siteMetadata.siteUrl,
+          feed_url: site.siteMetadata.feedUrl,
+          generator: 'GatsbyJS'
+        }),
+        feeds: [
+          {
+            output: FEED_FILE,
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 100,
+                  sort: {
+                    order: DESC,
+                    fields: [
+                      frontmatter___date
+                    ]
+                  }
+                ) {
+                  edges {
+                    node {
+                      html
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        description
+                        date
+                        title
+                        keywords
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            serialize: ({
+              query: {
+                site,
+                allMarkdownRemark
+              }
+            }) => allMarkdownRemark.edges.map(edge => ({
+              description: edge.node.description,
+              date: edge.node.frontmatter.date,
+              title: edge.node.frontmatter.title,
+              author: site.siteMetadata.author.fullName,
+              categories: edge.node.frontmatter.keywords.split(', '),
+              url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+              custom_elements: [
+                {
+                  'content:encoded': edge.node.html
+                }
+              ]
+            }))
+          }
+        ]
+      }
+    },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
@@ -46,9 +132,6 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
-    'gatsby-transformer-remark',
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
