0058d65c9f8138a8a6e991912934a061971a0aba
Emanuel Freitas <efreitasn@hotmail.com>
2019-03-30 20:18:01 -0300

    Change from TypeScript@4.1.0 to TypeScript@3.3.4000
    Due to issues related to styled-components being slow in the latest
    stable version of TypeScript(4.1.0), the version of this package had to
    be changed.
    
    Also, a change was made in the src/styles/breakpoints.ts file regarding
    the fact that, in v3.3.4000, TypeScript didn't consider ReadOnlyArray,
    such as TemplateStringsArray, as a valid type for rest parameters.

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,14 +1,14 @@
 import { css } from 'styled-components';
 
 // 700px
-export const bk1 = (...args: TemplateStringsArray) => css`
+export const bk1 = (...args: any) => css`
   @media (max-width: 43.75em) {
     ${args}
   }
 `;
 
 // 450px
-export const bk2 = (...args: TemplateStringsArray) => css`
+export const bk2 = (...args: any) => css`
   @media (max-width: 28.125em) {
     ${args}
   }
